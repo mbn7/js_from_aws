10e56810d00f0af5617ec01d6c040c8f796b4975
mbn7 <82445552+mbn7@users.noreply.github.com>
2022-12-04 17:18:30 +0200

    Update tst.js

--- tst.js
+++ tst.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.get('/',(req,res)=>{
     // return res.send();
-    return res.json({message: 'Server OK'})
+    return res.json({message: 'get server req'})
 })
 
-app.listen(3333)
+app.listen(3333,()=> {
+    console.log('listen on port: 3333')
+});
